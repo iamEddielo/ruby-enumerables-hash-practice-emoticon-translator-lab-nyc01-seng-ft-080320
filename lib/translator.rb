ce8e0a565132d5e58c180b5460a8692219b906d9
Eddie Lopez <iameddielopez@gmail.com>
2020-07-24 20:35:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ def get_japanese_emoticon(emoji_file,emoticon)
   emoji_hash.each do |key, value|
     #binding.pry
     if value[:english] == emoticon
-      return value[:english]
+      return value[:japanese]
     end
   end
   return "Sorry, that emoticon was not found"
